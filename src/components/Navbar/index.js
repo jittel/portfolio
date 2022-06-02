b474ad9d683b15e3cfc7cd9307edc2d95cc97827
jittel <nathan.c.ginn@gmail.com>
2022-06-01 17:16:15 -0700

    footer and navbar component files, navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <div>
+            <header>
+                <h1>Nate Ginn</h1>
+                <p>this is my portfolio</p>
+            </header>
+
+            <nav>
+                <a href="#">about</a>
+                <a href="./contact.html">contact</a>
+                <a href="./works.html">works</a>
+            </nav>
+        </div>
+    )
+}
